de355873c58e4f34f1aaa8381a7d4b6c17ef8489
Charles Oh <ogoons@hanmail.net>
2019-04-17 20:37:17 +0900

    Update

--- app/src/main/java/com/ogoons/coroutinesample/MainActivity.kt
+++ app/src/main/java/com/ogoons/coroutinesample/MainActivity.kt
@@ -2,33 +2,65 @@ package com.ogoons.coroutinesample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var tvCount: TextView
+    private lateinit var btnStart: Button
+    private lateinit var btnStop: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setup()
+
+        tvCount = findViewById(R.id.tv_count)
+        btnStart = findViewById(R.id.btn_start)
+        btnStop = findViewById(R.id.btn_stop)
+
+        btnStart.setOnClickListener { startCount() }
+        btnStop.setOnClickListener { stopCount() }
+
+        testLongRunningCoroutine()
     }
 
-    private fun setup() {
-        val job = GlobalScope.launch(Dispatchers.IO) {
-            // launch는 코루틴의 빌더
-            delay(1000)
-            println("world")
-            repeat(100) {
+    private var countJob: Job? = null
+
+    private var timeCount: Int = 0
+
+    private fun startCount() {
+        tvCount.text = timeCount.toString()
+        countJob = GlobalScope.launch {
+            while (isActive) {
                 delay(1000)
-                println(System.currentTimeMillis())
+                runOnUiThread {
+                    tvCount.text = (++timeCount).toString()
+                }
             }
         }
-        job.start()
-        Thread.sleep(2000)
-        println("Hello, ")
+    }
 
-        runBlocking {
+    private fun stopCount() {
+        countJob?.cancel()
+    }
 
+    /**
+     * 메인 스레드에서 실행
+     */
+    fun testLongRunningCoroutine() = runBlocking<Unit> {
+        val job = launch(Dispatchers.IO) {
+            repeat(10) { i ->
+                println("I'm sleeping $i ...")
+                delay(500L)
+            }
         }
-
+        delay(1300L) // just quit after delay
+        println("Not yet")
+        job.join()
+        println("Complete")
     }
 }
