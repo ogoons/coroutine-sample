dcc9cbc42de5d7c1ce27a05e9e8f1789770c0c21
Charles Oh <ogoons@hanmail.net>
2019-04-25 18:59:09 +0900

    Update

--- app/src/main/java/com/ogoons/coroutinesample/MainActivity.kt
+++ app/src/main/java/com/ogoons/coroutinesample/MainActivity.kt
@@ -114,4 +114,21 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
             println("I'm working... count : $it")
         }
     }
+
+    fun main(args: Array<String>) = runBlocking {
+        launch {
+            delay(200L)
+            println("Task from runBlocking")
+        }
+
+        coroutineScope {
+            launch {
+                delay(500L)
+                println("Task from nested launch")
+            }
+            delay(100L)
+            println("Task from coroutine scope")
+        }
+        println("Coroutine scope is over")
+    }
 }
